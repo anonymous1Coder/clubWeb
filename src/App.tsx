0d9fdf289b36a7ab0b5408b5f53f73beef06e27d
anonymous1Coder <dujinhua2021@gmail.com>
2025-08-26 16:28:37 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -460,7 +460,7 @@ export default function App() {
               </button>
             </div>
             <a
-              href="https:///clubweb.site/"
+              href="https://anonymous1coder.github.io/clubWeb/"
               target="_blank"
               rel="noreferrer"
               className="text-sm underline hover:text-indigo-600 transition-colors"
