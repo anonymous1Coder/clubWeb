c80a8c27e306540e3d76821d5ec5c195c948acbe
anonymous1Coder <dujinhua2021@gmail.com>
2025-08-26 17:23:47 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 export default defineConfig({
     plugins: [react()],
-    base: '/clubWeb/',
+    base: '/',
     resolve: {
         alias: { '@': resolve(__dirname, 'src') },
     },
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: '/clubWeb/',
+  base: '/',
   resolve: {
     alias: { '@': resolve(__dirname, 'src') },
   },
